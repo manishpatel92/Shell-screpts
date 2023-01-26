a3193c5a749450d2fdab56d054bd502980d12f93
Manish Patel <107122411+manishpatel92@users.noreply.github.com>
2023-01-26 18:35:49 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -15,13 +15,13 @@ sudo rm -rf output1.txt
 # for the step of git stages
 
 git init
-git config --global user.name "manish patel"
+git config --global user.name "manishpatel92"
+git config --global user.password "your-password"
+git config --global user.password "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"
+
+
 git config --global user.email "manish080194@gmail.com"
 git add .
 git commit -m "that is commit form shell"
 git remote add origin https://github.com/manishpatel92/Shell-screpts.git
 git push -u origin master
-
-echo "manishpatel92"
-
-echo "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"
